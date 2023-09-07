63cde24d36abdf1b06c1753aa072676fd4172504
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2023-09-07 17:48:51 -0300

    create page and index.css, index,js

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgKlQI83DcUQsnmbs1UgZ2X8PFXdIqEa8",
+    authDomain: "controledegastos-e39a4.firebaseapp.com",
+    projectId: "controledegastos-e39a4",
+    storageBucket: "controledegastos-e39a4.appspot.com",
+    messagingSenderId: "151727210545",
+    appId: "1:151727210545:web:df30f5399d0c14a1d3d510"
+};
+firebase.initializeApp(firebaseConfig)
